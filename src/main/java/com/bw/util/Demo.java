23577cc84de8e74178e2b3df8eb5714e55ab6e4c
BW05FanXingYue <431624476@qq.com>
2019-09-08 19:42:15 +0800

    第一次修改

---
+++ src/main/java/com/bw/util/Demo.java
@@ -0,0 +1,11 @@
+package com.bw.util;
+/** 
+* @author 作者：范星月
+* @version 创建时间：2019年9月8日 下午7:41:24
+* 类功能说明 
+*/
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("测试");
+	}
+}
